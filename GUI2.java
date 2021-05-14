1bf08c9f9c94d365d9377c56e58b1ee132e14283
SamySCode <83658601+SamySCode@users.noreply.github.com>
2021-05-14 21:10:25 +0100

    Add files via upload
    Samuel Asare made some changes.

--- GUI2.java
+++ GUI2.java
@@ -3,7 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ourfirstgui;
+
+
+/**
+ *
+ * @author samuelasare
+ */
+
 
 /**
  *
@@ -20,51 +26,51 @@ import javax.swing.JTextArea;
 public class GUI2 {
 
  JFrame frame = new JFrame();
-JLabel pageLabel = new JLabel("");
-private JLabel studentLabel = new JLabel("Students");
-private JLabel projectLabel = new JLabel("Projects");
-private JLabel studentsProjectLabel = new JLabel("Students Allocated");
+JLabel NameOfPage = new JLabel("");
+private JLabel Listofstudent = new JLabel("Students");
+private JLabel Listofprojects = new JLabel("Projects");
+private JLabel ListofStudentsAllocated = new JLabel("Students Allocated");
 
- private JTextArea studentstextfield = new JTextArea();
-private JTextArea projectstextfield = new JTextArea();
-private JTextArea studentsallocatedtextfield = new JTextArea();
+ private JTextArea textboxstudents = new JTextArea();
+private JTextArea textboxprojects = new JTextArea();
+private JTextArea textboxStudentsAllocated = new JTextArea();
 
  GUI2(){
-pageLabel.setBounds(0, 0, 200, 35);
-pageLabel.setFont(new Font(null, Font.PLAIN, 25));
+NameOfPage.setBounds(0, 0, 200, 35);
+NameOfPage.setFont(new Font(null, Font.PLAIN, 25));
 
- studentLabel.setBounds(400, 30, 250, 35);
-studentLabel.setFont(new Font(null, Font.BOLD, 20));
+ Listofstudent.setBounds(400, 30, 250, 35);
+Listofstudent.setFont(new Font(null, Font.BOLD, 20));
 
- projectLabel.setBounds(100, 30, 250, 35);
-projectLabel.setFont(new Font(null, Font.BOLD, 20));
+ Listofprojects.setBounds(100, 30, 250, 35);
+Listofprojects.setFont(new Font(null, Font.BOLD, 20));
 
- studentsProjectLabel.setBounds(700, 30, 250, 35);
-studentsProjectLabel.setFont(new Font(null, Font.BOLD, 20));
+ ListofStudentsAllocated.setBounds(700, 30, 250, 35);
+ListofStudentsAllocated.setFont(new Font(null, Font.BOLD, 20));
 
- studentstextfield.setBounds(400, 80, 230, 400);
-studentstextfield.setForeground(Color.black);
-studentstextfield.setFont(new Font("Arial", Font.ITALIC, 14));
-studentstextfield.setLineWrap(true);
-studentstextfield.setRows(10);
+ textboxstudents.setBounds(400, 80, 230, 400);
+textboxstudents.setForeground(Color.black);
+textboxstudents.setFont(new Font("Arial", Font.ITALIC, 14));
+textboxstudents.setLineWrap(true);
+textboxstudents.setRows(10);
 
- projectstextfield.setBounds(100, 80, 230, 400);
-projectstextfield.setForeground(Color.BLACK);
-projectstextfield.setFont(new Font("Arial", Font.ITALIC, 14));
-projectstextfield.setLineWrap(true);
+ textboxprojects.setBounds(100, 80, 230, 400);
+textboxprojects.setForeground(Color.BLACK);
+textboxprojects.setFont(new Font("Arial", Font.ITALIC, 14));
+textboxprojects.setLineWrap(true);
 
- studentsallocatedtextfield.setBounds(700, 80, 230, 400);
-studentsallocatedtextfield.setForeground(Color.BLACK);
-studentsallocatedtextfield.setFont(new Font("Arial", Font.ITALIC, 14));
-studentsallocatedtextfield.setLineWrap(true);
+ textboxStudentsAllocated.setBounds(700, 80, 230, 400);
+textboxStudentsAllocated.setForeground(Color.BLACK);
+textboxStudentsAllocated.setFont(new Font("Arial", Font.ITALIC, 14));
+textboxStudentsAllocated.setLineWrap(true);
 
- frame.add(studentsallocatedtextfield);
-frame.add(projectstextfield);
-frame.add(studentstextfield);
-frame.add(studentsProjectLabel);
-frame.add(studentLabel);
-frame.add(projectLabel);
-frame.add(pageLabel);
+ frame.add(textboxStudentsAllocated);
+frame.add(textboxprojects);
+frame.add(textboxstudents);
+frame.add(ListofStudentsAllocated);
+frame.add(Listofstudent);
+frame.add(Listofprojects);
+frame.add(NameOfPage);
 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 frame.setSize(1000, 800);
 frame.setLayout(null);
